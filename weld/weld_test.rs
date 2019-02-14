6b212fd9a6ceb09e7f8b945ae75568f32b6428d6
Colin Merkel <colin353@gmail.com>
2019-02-13 17:32:01 -0800

    Add weld_test library

---
+++ weld/weld_test.rs
@@ -0,0 +1,48 @@
+extern crate largetable_test;
+extern crate weld;
+extern crate weld_server_lib;
+
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct WeldServerTestClient {
+    client: Arc<weld_server_lib::WeldServiceHandler<largetable_test::LargeTableMockClient>>,
+    username: String,
+}
+
+impl WeldServerTestClient {
+    pub fn new(username: String) -> Self {
+        let db = largetable_test::LargeTableMockClient::new();
+        WeldServerTestClient {
+            client: Arc::new(weld_server_lib::WeldServiceHandler::new(db)),
+            username: username,
+        }
+    }
+}
+
+impl weld::WeldServer for WeldServerTestClient {
+    fn read(&self, req: weld::FileIdentifier) -> weld::File {
+        self.client.read(req)
+    }
+    fn submit(&self, req: weld::Change) -> weld::SubmitResponse {
+        self.client.submit(&self.username, req.get_id())
+    }
+    fn snapshot(&self, req: weld::Change) -> weld::SnapshotResponse {
+        self.client.snapshot(&self.username, req)
+    }
+    fn get_change(&self, req: weld::Change) -> weld::Change {
+        self.client.get_change(req)
+    }
+    fn list_changes(&self) -> Vec<weld::Change> {
+        self.client
+            .list_changes(&self.username)
+            .take_changes()
+            .into_vec()
+    }
+    fn get_latest_change(&self) -> weld::Change {
+        self.client.get_latest_change()
+    }
+    fn list_files(&self, req: weld::FileIdentifier) -> Vec<weld::File> {
+        self.client.list_files(req).take_files().into_vec()
+    }
+}
