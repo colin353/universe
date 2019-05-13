18e97f385dcac80822943ccaf6a5479790ce80af
Colin Merkel <colin353@gmail.com>
2019-05-12 18:29:26 -0700

    Relax constraints on server to only Clone

--- util/ws/ws.rs
+++ util/ws/ws.rs
@@ -6,15 +6,17 @@ pub use hyper::Body;
 pub type Request = hyper::Request<Body>;
 pub type Response = hyper::Response<Body>;
 
-pub trait Server: Sync + Send + Clone + Copy + 'static {
+pub trait Server: Sync + Send + Clone + 'static {
     fn respond(&self, path: String, Request) -> Response;
 
     fn serve(self, port: u16) {
         let addr = ([127, 0, 0, 1], port).into();
+        let self_clone = self.clone();
         let server = hyper::Server::bind(&addr)
             .serve(move || {
+                let s = self_clone.clone();
                 hyper::service::service_fn_ok(move |req: Request| {
-                    self.respond(req.uri().path().into(), req)
+                    s.respond(req.uri().path().into(), req)
                 })
             })
             .map_err(|_| ());;
