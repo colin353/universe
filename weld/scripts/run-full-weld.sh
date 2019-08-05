c3829a9792c53ddb3e5b380943f1928d334bfab4
Colin Merkel <colinmerkel@gmail.com>
2019-08-05 09:16:55 -0700

    Add sleep to weld startup script

--- weld/scripts/run-full-weld.sh
+++ weld/scripts/run-full-weld.sh
@@ -2,13 +2,21 @@
 
 IP_ADDR=$1
 
+sudo umount -f $HOME/codefs
+
+sleep 0.5
+
 RUST_LOG=debug RUST_BACKTRACE=1 bazel run //largetable:largetable_server -- \
   --data_directory="/usr/local/largetable/" &
+
+sleep 0.5
 
 RUST_BACKTRACE=1 bazel run //weld:weld_server -- \
   --root_cert="/home/colin/Documents/scratch/certs/root.crt" \
   --pkcs12="/home/colin/Documents/scratch/certs/server.p12" \
   --use_mock_largetable=true &
+
+sleep 0.5
 
 sudo umount -l /home/colinmerkel/codefs
 
@@ -18,6 +26,8 @@ RUST_BACKTRACE=1 bazel run //weld:weld_client -- \
   --cert="/home/colin/Documents/scratch/certs/client.p12" \
   --weld_hostname=127.0.0.1 \
   --mount_point=/home/colin/codefs &
+
+sleep 0.5
 
 RUST_BACKTRACE=1 bazel run //weld/review -- \
   --use_tls=true \
