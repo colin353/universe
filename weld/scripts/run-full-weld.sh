579f5b63d15191cf121c0ff15f79ef4c795479e9
Colin Merkel <colinmerkel@gmail.com>
2019-08-05 14:58:21 -0400

    Increase delay when starting weld server

--- weld/scripts/run-full-weld.sh
+++ weld/scripts/run-full-weld.sh
@@ -2,23 +2,21 @@
 
 IP_ADDR=$1
 
-sudo umount -f $HOME/codefs
-
-sleep 0.5
+sleep 1
 
 RUST_LOG=debug RUST_BACKTRACE=1 bazel run //largetable:largetable_server -- \
   --data_directory="/usr/local/largetable/" &
 
-sleep 0.5
+sleep 1
 
 RUST_BACKTRACE=1 bazel run //weld:weld_server -- \
   --root_cert="/home/colin/Documents/scratch/certs/root.crt" \
   --pkcs12="/home/colin/Documents/scratch/certs/server.p12" \
   --use_mock_largetable=true &
 
-sleep 0.5
+sleep 1
 
-sudo umount -l /home/colinmerkel/codefs
+sudo umount -l $HOME/codefs
 
 RUST_BACKTRACE=1 bazel run //weld:weld_client -- \
   --use_tls=true \
@@ -27,7 +25,7 @@ RUST_BACKTRACE=1 bazel run //weld:weld_client -- \
   --weld_hostname=127.0.0.1 \
   --mount_point=/home/colin/codefs &
 
-sleep 0.5
+sleep 1
 
 RUST_BACKTRACE=1 bazel run //weld/review -- \
   --use_tls=true \
