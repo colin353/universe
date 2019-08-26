b0f31779e02fc1c0d3c13b2902805de322cdaa0a
Colin Merkel <colinmerkel@gmail.com>
2019-08-26 19:16:57 -0400

    delete useless shit

--- third_party/vendor/schannel-0.1.15/src/key_handle.rs
+++
@@ -1,5 +0,0 @@
-//! Deprecated.
-#![deprecated(note = "use cert_context::PrivateKey", since = "0.1.5")]
-
-#[deprecated(note = "use cert_context::PrivateKey", since = "0.1.5")]
-pub use cert_context::PrivateKey as KeyHandle;
