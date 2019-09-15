570da345b724f1b733b2f8b3ae8d7ad618c2fd61
Colin Merkel <colinmerkel@gmail.com>
2019-09-15 15:26:09 -0400

    Make BatchingClient threaded

--- largetable/batching_client.rs
+++ largetable/batching_client.rs
@@ -1,25 +1,33 @@
 use std::collections::BTreeMap;
 use std::collections::VecDeque;
+use std::sync::RwLock;
 
 struct BatchingClient<T, C: largetable_client::LargeTableClient> {
-    cache: VecDeque<BTreeMap<(String, String), T>>,
+    cache: RwLock<VecDeque<BTreeMap<(String, String), T>>>,
     client: C,
 }
 
 impl<T: protobuf::Message + Clone, C: largetable_client::LargeTableClient> BatchingClient<T, C> {
     fn new(client: C) -> Self {
         Self {
-            cache: VecDeque::new(),
+            cache: RwLock::new(VecDeque::new()),
             client: client,
         }
     }
 
-    fn add_cache(&mut self) {
-        self.cache.push_back(BTreeMap::new());
+    fn new_with_cache(client: C) -> Self {
+        Self {
+            cache: RwLock::new(VecDeque::from(vec![BTreeMap::new()])),
+            client: client,
+        }
+    }
+
+    fn add_cache(&self) {
+        self.cache.write().unwrap().push_back(BTreeMap::new());
     }
 
     fn read(&self, row: &str, col: &str) -> Option<T> {
-        for cache in self.cache.iter().rev() {
+        for cache in self.cache.read().unwrap().iter().rev() {
             if let Some(x) = cache.get(&(row.to_owned(), col.to_owned())) {
                 return Some(x.clone());
             }
@@ -34,15 +42,16 @@ impl<T: protobuf::Message + Clone, C: largetable_client::LargeTableClient> Batch
     }
 
     fn prepare_flush(&mut self) {
-        self.cache.push_back(BTreeMap::new());
+        self.cache.write().unwrap().push_back(BTreeMap::new());
     }
 
     fn finish_flush(&mut self) {
-        self.cache.pop_front();
+        self.cache.write().unwrap().pop_front();
     }
 
     fn perform_flush(&self) {
-        let cache = match self.cache.front() {
+        let cache_locked = self.cache.read().unwrap();
+        let cache = match cache_locked.front() {
             Some(x) => x,
             None => return,
         };
@@ -54,8 +63,8 @@ impl<T: protobuf::Message + Clone, C: largetable_client::LargeTableClient> Batch
         writer.finish(&self.client);
     }
 
-    fn write(&mut self, row: &str, col: &str, message: T) {
-        if let Some(cache) = self.cache.back_mut() {
+    fn write(&self, row: &str, col: &str, message: T) {
+        if let Some(cache) = self.cache.write().unwrap().back_mut() {
             cache.insert((row.to_owned(), col.to_owned()), message);
             return;
         }
@@ -93,7 +102,7 @@ mod tests {
         let mut batching_client = BatchingClient::new(client);
         batching_client.add_cache();
         batching_client.write("test_row", "test_col", largetable_client::Record::new());
-        batching_client.flush();
+        batching_client.prepare_flush();
         batching_client.read("test_row", "test_col").unwrap();
     }
 }
