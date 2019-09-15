ae7bd8d15cb3e982841f0cf34ed27eb26bd01f3c
Colin Merkel <colinmerkel@gmail.com>
2019-09-15 15:17:26 -0400

    Create batching client

---
+++ largetable/batching_client.rs
@@ -0,0 +1,99 @@
+use std::collections::BTreeMap;
+use std::collections::VecDeque;
+
+struct BatchingClient<T, C: largetable_client::LargeTableClient> {
+    cache: VecDeque<BTreeMap<(String, String), T>>,
+    client: C,
+}
+
+impl<T: protobuf::Message + Clone, C: largetable_client::LargeTableClient> BatchingClient<T, C> {
+    fn new(client: C) -> Self {
+        Self {
+            cache: VecDeque::new(),
+            client: client,
+        }
+    }
+
+    fn add_cache(&mut self) {
+        self.cache.push_back(BTreeMap::new());
+    }
+
+    fn read(&self, row: &str, col: &str) -> Option<T> {
+        for cache in self.cache.iter().rev() {
+            if let Some(x) = cache.get(&(row.to_owned(), col.to_owned())) {
+                return Some(x.clone());
+            }
+        }
+        self.client.read_proto::<T>(row, col, 0)
+    }
+
+    fn flush(&mut self) {
+        self.prepare_flush();
+        self.perform_flush();
+        self.finish_flush();
+    }
+
+    fn prepare_flush(&mut self) {
+        self.cache.push_back(BTreeMap::new());
+    }
+
+    fn finish_flush(&mut self) {
+        self.cache.pop_front();
+    }
+
+    fn perform_flush(&self) {
+        let cache = match self.cache.front() {
+            Some(x) => x,
+            None => return,
+        };
+
+        let mut writer = largetable_client::LargeTableBatchWriter::new();
+        for ((row, col), msg) in cache {
+            writer.write_proto(row, col, 0, msg);
+        }
+        writer.finish(&self.client);
+    }
+
+    fn write(&mut self, row: &str, col: &str, message: T) {
+        if let Some(cache) = self.cache.back_mut() {
+            cache.insert((row.to_owned(), col.to_owned()), message);
+            return;
+        }
+
+        self.client.write_proto::<T>(row, col, 0, &message);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate largetable_test;
+
+    #[test]
+    fn test_write() {
+        let client = largetable_test::LargeTableMockClient::new();
+        let mut batching_client = BatchingClient::new(client);
+
+        batching_client.write("test_row", "test_col", largetable_client::Record::new());
+        batching_client.read("test_row", "test_col").unwrap();
+    }
+
+    #[test]
+    fn test_write_with_cache() {
+        let client = largetable_test::LargeTableMockClient::new();
+        let mut batching_client = BatchingClient::new(client);
+        batching_client.add_cache();
+        batching_client.write("test_row", "test_col", largetable_client::Record::new());
+        batching_client.read("test_row", "test_col").unwrap();
+    }
+
+    #[test]
+    fn test_write_with_flush() {
+        let client = largetable_test::LargeTableMockClient::new();
+        let mut batching_client = BatchingClient::new(client);
+        batching_client.add_cache();
+        batching_client.write("test_row", "test_col", largetable_client::Record::new());
+        batching_client.flush();
+        batching_client.read("test_row", "test_col").unwrap();
+    }
+}
