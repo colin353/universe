ca082500a860c32f99195a751234e85a4dc1e864
Colin Merkel <colinmerkel@gmail.com>
2019-10-17 21:46:27 -0400

    second

--- auth/auth_service_impl.rs
+++ auth/auth_service_impl.rs
@@ -1,7 +1,34 @@
-pub struct AuthServiceHandler {}
-impl AuthServiceHandler {
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+struct LoginRecord {
+    username: String,
+    valid: bool,
+}
+impl LoginRecord {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            username: String::new(),
+            valid: false,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.valid
+    }
+}
+
+#[derive(Clone)]
+pub struct AuthServiceHandler {
+    hostname: String,
+    tokens: Arc<RwLock<HashMap<String, LoginRecord>>>,
+}
+impl AuthServiceHandler {
+    pub fn new(hostname: String) -> Self {
+        Self {
+            hostname: hostname,
+            tokens: Arc::new(RwLock::new(HashMap::new())),
+        }
     }
 }
 
@@ -19,6 +46,13 @@ impl auth_grpc_rust::AuthenticationService for AuthServiceHandler {
         _m: grpc::RequestOptions,
         req: auth_grpc_rust::AuthenticateRequest,
     ) -> grpc::SingleResponse<auth_grpc_rust::AuthenticateResponse> {
-        grpc::SingleResponse::completed(auth_grpc_rust::AuthenticateResponse::new())
+        let mut response = auth_grpc_rust::AuthenticateResponse::new();
+        if let Some(t) = self.tokens.read().unwrap().get(req.get_token()) {
+            if t.is_valid() {
+                response.set_success(true);
+                response.set_username(t.username.clone());
+            }
+        }
+        grpc::SingleResponse::completed(response)
     }
 }
