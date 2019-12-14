a2c9dc2385adb793545140e27ffed473fe66b68b
Colin Merkel <colinmerkel@gmail.com>
2019-12-14 09:10:51 -0500

    task-spawn

--- tasks/server_impl.rs
+++ tasks/server_impl.rs
@@ -1,4 +1,5 @@
 extern crate grpc;
+extern crate protobuf;
 extern crate tokio;
 
 extern crate futures;
@@ -9,7 +10,6 @@ extern crate tasks_grpc_rust;
 use futures::sync::mpsc::{unbounded, UnboundedSender};
 use futures::Stream;
 use largetable_client::LargeTableClient;
-use std::sync::{Arc, Mutex};
 use task_lib::TaskManager;
 use tasks_grpc_rust::{Status, TaskArgument, TaskStatus};
 use tokio::prelude::{future, Future};
@@ -68,14 +68,29 @@ impl<C: LargeTableClient + Clone + Send + 'static> TaskServiceHandler<C> {
         &self,
         req: tasks_grpc_rust::GetStatusRequest,
     ) -> tasks_grpc_rust::TaskStatus {
-        match self.database.read_proto(TASK_STATUS, req.get_task_id(), 0) {
-            Some(s) => s,
-            None => {
-                let mut s = TaskStatus::new();
-                s.set_status(Status::DOES_NOT_EXIST);
-                s
-            }
+        let mut status: TaskStatus =
+            match self.database.read_proto(TASK_STATUS, req.get_task_id(), 0) {
+                Some(s) => s,
+                None => {
+                    let mut s = TaskStatus::new();
+                    s.set_status(Status::DOES_NOT_EXIST);
+                    return s;
+                }
+            };
+
+        let iterator = largetable_client::LargeTableScopedIterator::new(
+            &self.database,
+            String::from(TASK_STATUS),
+            format!("s{}/", req.get_task_id()),
+            String::new(),
+            String::new(),
+            0,
+        );
+        for (_, subtask_status) in iterator {
+            status.mut_subtasks().push(subtask_status);
         }
+
+        status
     }
 
     pub fn begin_task(&self, task_id: String) -> TaskFuture {
@@ -118,11 +133,11 @@ impl<C: LargeTableClient + Clone + Send + 'static> tasks_grpc_rust::TaskService
     }
 }
 
-struct Manager<C: LargeTableClient + 'static> {
+struct Manager<C: LargeTableClient + Clone + 'static> {
     task_id: String,
     database: C,
 }
-impl<C: LargeTableClient + 'static> Manager<C> {
+impl<C: LargeTableClient + Clone + 'static> Manager<C> {
     pub fn new(task_id: String, database: C) -> Self {
         Self {
             task_id: task_id,
@@ -131,7 +146,7 @@ impl<C: LargeTableClient + 'static> Manager<C> {
     }
 }
 
-impl<C: LargeTableClient + 'static> task_lib::TaskManager for Manager<C> {
+impl<C: LargeTableClient + Clone + 'static> task_lib::TaskManager for Manager<C> {
     fn get_status(&self) -> TaskStatus {
         self.database
             .read_proto(TASK_STATUS, &self.task_id, 0)
@@ -140,6 +155,21 @@ impl<C: LargeTableClient + 'static> task_lib::TaskManager for Manager<C> {
     fn set_status(&self, status: TaskStatus) {
         self.database
             .write_proto(TASK_STATUS, &self.task_id, 0, &status);
+    }
+    fn spawn(&self, task_name: &str, arguments: Vec<TaskArgument>) -> task_lib::TaskResultFuture {
+        let subtask_id = self
+            .database
+            .reserve_id(TASK_IDS, &self.task_id)
+            .to_string();
+        let task_id = format!("s{}/{}", self.task_id, subtask_id);
+        let mut status = TaskStatus::new();
+        status.set_name(task_name.to_owned());
+        status.set_arguments(protobuf::RepeatedField::from_vec(arguments));
+        status.set_task_id(task_id.clone());
+        self.database.write_proto(TASK_STATUS, &task_id, 0, &status);
+
+        let m = Manager::new(task_id.clone(), self.database.clone());
+        m.run(status)
     }
     fn run(self, mut status: TaskStatus) -> task_lib::TaskResultFuture {
         let task = match task_lib::TASK_REGISTRY.get(status.get_name()) {
@@ -180,5 +210,48 @@ mod tests {
         let status = handler.get_status(req);
         assert_eq!(status.get_status(), Status::SUCCESS);
         assert_eq!(status.get_task_id(), "1");
+    }
+
+    #[test]
+    fn test_task_spawning_fails_with_no_arguments() {
+        let handler = setup_task_runner();
+        let mut req = tasks_grpc_rust::CreateTaskRequest::new();
+        req.set_task_name(String::from("spawner"));
+
+        let status = handler.create_task(req);
+        assert_eq!(status.get_task_id(), "1");
+
+        std::thread::sleep(std::time::Duration::from_millis(50));
+
+        let mut req = tasks_grpc_rust::GetStatusRequest::new();
+        req.set_task_id(status.get_task_id().to_owned());
+        let status = handler.get_status(req);
+
+        assert_eq!(status.get_status(), Status::FAILURE);
+        assert_eq!(status.get_reason(), "not enough arguments");
+        assert_eq!(status.get_task_id(), "1");
+    }
+
+    #[test]
+    fn test_task_succeeds_spawning() {
+        let handler = setup_task_runner();
+        let mut req = tasks_grpc_rust::CreateTaskRequest::new();
+        req.set_task_name(String::from("spawner"));
+        let mut args = task_lib::ArgumentsBuilder::new();
+        args.add_string("subtask", String::from("noop"));
+        *req.mut_arguments() = protobuf::RepeatedField::from_vec(args.build());
+
+        let status = handler.create_task(req);
+        assert_eq!(status.get_task_id(), "1");
+
+        std::thread::sleep(std::time::Duration::from_millis(50));
+
+        let mut req = tasks_grpc_rust::GetStatusRequest::new();
+        req.set_task_id(status.get_task_id().to_owned());
+        let status = handler.get_status(req);
+
+        assert_eq!(status.get_status(), Status::SUCCESS);
+        assert_eq!(status.get_subtasks().len(), 1);
+        assert_eq!(status.get_subtasks()[0].get_task_id(), "s1/1");
     }
 }
--- tasks/task_lib.rs
+++ tasks/task_lib.rs
@@ -11,6 +11,7 @@ lazy_static! {
     pub static ref TASK_REGISTRY: HashMap<&'static str, Box<dyn Task>> = {
         let mut m: HashMap<&'static str, Box<dyn Task>> = HashMap::new();
         m.insert("noop", Box::new(Noop::new()));
+        m.insert("spawner", Box::new(Spawner::new()));
         m
     };
 }
@@ -20,11 +21,64 @@ pub type TaskResultFuture = Box<dyn Future<Item = TaskStatus, Error = ()> + Send
 pub trait TaskManager {
     fn set_status(&self, status: TaskStatus);
     fn get_status(&self) -> TaskStatus;
+    fn spawn(&self, task_name: &str, arguments: Vec<TaskArgument>) -> TaskResultFuture;
     fn run(self, mut status: TaskStatus) -> TaskResultFuture;
+    fn failure(&self, mut status: TaskStatus, reason: &str) -> TaskResultFuture {
+        status.set_status(Status::FAILURE);
+        status.set_reason(reason.to_owned());
+        Box::new(future::ok(status))
+    }
+
+    fn success(&self, mut status: TaskStatus) -> TaskResultFuture {
+        status.set_status(Status::SUCCESS);
+        Box::new(future::ok(status))
+    }
 }
 
 pub trait Task: Sync + 'static {
     fn run(&self, args: &[TaskArgument], manager: Box<dyn TaskManager>) -> TaskResultFuture;
+}
+
+pub struct ArgumentsBuilder {
+    args: Vec<TaskArgument>,
+}
+
+impl ArgumentsBuilder {
+    pub fn new() -> Self {
+        Self { args: Vec::new() }
+    }
+
+    pub fn add_string(&mut self, name: &str, value: String) {
+        let mut a = TaskArgument::new();
+        a.set_name(name.to_owned());
+        a.set_value_string(value);
+        self.args.push(a)
+    }
+
+    pub fn add_int(&mut self, name: &str, value: i64) {
+        let mut a = TaskArgument::new();
+        a.set_name(name.to_owned());
+        a.set_value_int(value);
+        self.args.push(a)
+    }
+
+    pub fn add_float(&mut self, name: &str, value: f32) {
+        let mut a = TaskArgument::new();
+        a.set_name(name.to_owned());
+        a.set_value_float(value);
+        self.args.push(a)
+    }
+
+    pub fn add_bool(&mut self, name: &str, value: bool) {
+        let mut a = TaskArgument::new();
+        a.set_name(name.to_owned());
+        a.set_value_bool(value);
+        self.args.push(a)
+    }
+
+    pub fn build(self) -> Vec<TaskArgument> {
+        self.args
+    }
 }
 
 pub struct Noop {}
@@ -38,5 +92,23 @@ impl Task for Noop {
         let mut status = manager.get_status();
         status.set_status(Status::SUCCESS);
         Box::new(future::ok(status))
+    }
+}
+
+pub struct Spawner {}
+impl Spawner {
+    fn new() -> Self {
+        Self {}
+    }
+}
+impl Task for Spawner {
+    fn run(&self, args: &[TaskArgument], manager: Box<dyn TaskManager>) -> TaskResultFuture {
+        let mut status = manager.get_status();
+
+        if args.len() != 1 {
+            return manager.failure(status, "not enough arguments");
+        }
+
+        return manager.spawn(args[0].get_value_string(), Vec::new());
     }
 }
