5ecd4f25981faf799dba6feed23ffb813f76d5a4
Colin Merkel <colin353@gmail.com>
2020-05-18 15:44:23 -0400

    revert change

--- weld/fs.rs
+++ weld/fs.rs
@@ -917,9 +917,7 @@ impl<C: largetable_client::LargeTableClient> WeldFS<C> {
 
         let to_skip = if offset == 0 { offset } else { offset + 1 } as usize;
         for (i, entry) in entries.into_iter().enumerate().skip(to_skip) {
-            if reply.add(entry.inode, i as i64, entry.filetype, entry.name) {
-                break;
-            };
+            reply.add(entry.inode, i as i64, entry.filetype, entry.name);
         }
         reply.ok();
     }
