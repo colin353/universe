5563b1b3ac962e71a06b062c3c1725fc06e0c642
Colin Merkel <colinwm@github.com>
2023-04-06 12:25:34 -0400

    wip

--- homepage/main.rs
+++ homepage/main.rs
@@ -5,91 +5,46 @@ extern crate ws;
 use ws::{Body, Request, Response, Server};
 
 #[derive(Clone)]
-pub struct HomepageServer<A> {
+pub struct HomepageServer {
     static_dir: String,
-    auth: A,
     base_url: String,
 }
-impl<A> HomepageServer<A>
-where
-    A: auth_client::AuthServer,
-{
-    fn new(static_dir: String, base_url: String, auth: A) -> Self {
+impl HomepageServer {
+    fn new(static_dir: String, base_url: String) -> Self {
         Self {
             static_dir: static_dir,
-            auth: auth,
             base_url: base_url,
         }
     }
-
-    fn login(&self, token: &str) -> Response {
-        let result = self.auth.authenticate(token.to_owned());
-        if result.get_success() {
-            return Response::new(Body::from("you are logged"));
-        }
-
-        let result = self
-            .auth
-            .login_then_redirect(format!("{}/login-test", self.base_url));
-        let mut response = Response::new(Body::from("redirecting..."));
-        self.redirect(result.get_url(), &mut response);
-        response
-    }
-
-    fn login_test(&self, token: &str) -> Response {
-        let result = self.auth.authenticate(token.to_owned());
-        if result.get_success() {
-            return Response::new(Body::from("you are logged"));
-        }
-        Response::new(Body::from("you are NOT logged"))
-    }
 }
 
-impl<A> Server for HomepageServer<A>
-where
-    A: auth_client::AuthServer,
-{
+impl Server for HomepageServer {
     fn respond(&self, path: String, _req: Request, token: &str) -> Response {
-        if path.starts_with("/static/") {
-            return self.serve_static_files(path, "/static/", &self.static_dir);
+        if path == "/" || path.is_empty() {
+            return self.serve_static_files("/index.html".to_string(), "", &self.static_dir);
         }
 
-        if path == "/login-test" {
-            return self.login_test(token);
-        }
-
-        if path == "/login" {
-            return self.login(token);
-        }
-
-        Response::new(Body::from("hello world!"))
+        return self.serve_static_files(path, "", &self.static_dir);
     }
 }
 
 #[tokio::main]
 async fn main() {
     let port = define_flag!("port", 8080, "the port to bind to");
+    let base_url = define_flag!(
+        "base_url",
+        "https://colinmerkel.xyz".to_string(),
+        "the base URL of the website"
+    );
     let static_files = define_flag!(
         "static_files",
         String::from("/static/"),
         "the directory containing static files"
     );
-    let auth_hostname = define_flag!(
-        "auth_hostname",
-        String::from("127.0.0.1"),
-        "the hostname of the authentication service"
-    );
-    let base_url = define_flag!(
-        "base_url",
-        String::from("http://localhost:8080"),
-        "the base url to access this service"
-    );
-    let auth_port = define_flag!("auth_port", 8888, "the port of the authentication service");
-    parse_flags!(port, static_files, auth_hostname, auth_port, base_url);
+    parse_flags!(port, base_url, static_files);
 
-    let auth = auth_client::AuthClient::new(&auth_hostname.value(), auth_port.value());
     ws::serve(
-        HomepageServer::new(static_files.value(), base_url.value(), auth),
+        HomepageServer::new(static_files.value(), base_url.value()),
         port.value(),
     )
     .await;
